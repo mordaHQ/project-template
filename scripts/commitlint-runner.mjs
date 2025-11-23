ce29ae9c73544bff01eefd789e1f38dc0a1cd9e9
mordaHQ <devmorda@gmail.com>
2025-11-24 01:54:59 +0500

    fix: correct commitlint config to CJS

---
+++ scripts/commitlint-runner.mjs
@@ -0,0 +1,10 @@
+﻿import { readFileSync } from "fs";
+import { execa } from "execa";
+
+const msgFile = process.argv[2];
+const msg = readFileSync(msgFile, "utf8").trim();
+
+execa("npx", ["commitlint", "--color"], {
+  input: msg,
+  stdio: "inherit"
+});
