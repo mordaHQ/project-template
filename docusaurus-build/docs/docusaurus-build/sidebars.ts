610f3893fc279d07c2bfcea1dbb5517e269810a7
mordaHQ <devmorda@gmail.com>
2025-11-25 01:11:58 +0500

    fix: correct .gitignore for Docusaurus

---
+++ docusaurus-build/docs/docusaurus-build/sidebars.ts
@@ -0,0 +1,14 @@
+/**
+ * @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ */
+const sidebars = {
+  docs: [
+    {
+      type: "doc",
+      id: "intro", // это intro.md — главная страница
+      label: "Введение",
+    },
+  ],
+};
+
+export default sidebars;
