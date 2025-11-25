47fd7bf0944abff38393e9425cf6e510b2985df6
mordaHQ <mordaHQ@users.noreply.github.com>
2025-11-25 21:40:32 +0000

    deploy: 8d4ec64482c05c2900ee4b208da78b53facb9dd2

---
+++ assets/js/acecf23e.db172536.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocusaurus_build=globalThis.webpackChunkdocusaurus_build||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/project-template/blog","blogTitle":"Blog","authorsListPath":"/project-template/blog/authors"}')}}]);
---
+++ assets/js/eebfef7a.f9b4a101.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdocusaurus_build=globalThis.webpackChunkdocusaurus_build||[]).push([[704],{62:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/project-template/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
